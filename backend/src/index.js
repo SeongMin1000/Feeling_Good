50cea8a98cdf146cfde36439290fad8b91f8be7e
SeongMin1000 <russoll3579@gmail.com>
2025-05-21 22:42:24 +0900

    chore: Express 서버에 frontend 정적 경로 설정 추가

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,31 @@
 const express = require('express');
+const cors = require('cors'); // 🔹 CORS 모듈 import
 const app = express();
+const path = require('path');
 
-// 회원가입, 로그인 라우터
+require('dotenv').config({ path: path.join(__dirname, '../../.env') });
+
+
+// 라우터 import
 const authRoutes = require('./routes/authRoutes');
 
-const PORT = process.env.PORT || 3000;
+// 기본 포트 설정
+const PORT = process.env.PORT;
 
-app.use(express.json());
+// 미들웨어 설정
+app.use(cors()); // 🔹 프론트엔드 요청 허용
+app.use(express.json()); // 🔹 JSON 파싱
 
-// auth 라우터 연결
+// 라우터 등록
 app.use('/auth', authRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// 정적 파일 폴더 등록 (frontend)
+app.use(express.static(path.join(__dirname, '../../frontend/pages')));
+app.use('/js', express.static(path.join(__dirname, '../../frontend/js')));
+app.use('/css', express.static(path.join(__dirname, '../../frontend/css')));
 
-//dd
+
+// 서버 시작
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
